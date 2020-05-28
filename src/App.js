ce587f9aa231a7efdfae4fdde7dc0126d6b5f722
Alessio Rapini <alessio.rapini@yahoo.it>
2020-05-28 17:16:39 +0200

    Use React hooks

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import lottie from "lottie-web";
 import animationData from "./car.json";
 import "./App.css";
 
 let animObj = null;
 
-class App extends React.Component {
-  componentDidMount() {
-    console.log("componentDidMount");
+// class App extends React.Component {
+const App = () => {
+  const animationRef = useRef();
 
+  useEffect(() => {
     // Call the loadAnimation to start the animation
     animObj = lottie.loadAnimation({
-      container: this.animBox, // the dom element that will contain the animation
+      container: animationRef.current, // the dom element that will contain the animation
       renderer: "svg",
       loop: true,
       autoplay: true,
       animationData: animationData, // the path to the animation json
     });
-  }
+  }, []);
 
-  handleStop = () => {
+  const handleStop = () => {
     animObj.stop();
   };
 
-  handlePlay = () => {
+  const handlePlay = () => {
     animObj.play();
   };
 
-  render() {
-    return (
-      <div className="App">
-        <div
-          className="App-animation"
-          ref={(ref) => (this.animBox = ref)}
-        ></div>
-        <div className="App-buttons_wrapper">
-          <div className="App-button" onClick={this.handleStop}>
-            Stop
-          </div>
-          <div className="App-button" onClick={this.handlePlay}>
-            Play
-          </div>
+  return (
+    <div className="App">
+      <div className="App-animation" ref={animationRef}></div>
+      <div className="App-buttons_wrapper">
+        <div className="App-button" onClick={handleStop}>
+          Stop
+        </div>
+        <div className="App-button" onClick={handlePlay}>
+          Play
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
